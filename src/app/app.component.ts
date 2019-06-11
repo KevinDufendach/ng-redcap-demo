ea5f86797ffc85f7818a4ba82f4ecd3cda0c454a
Kevin Dufendach <Kevin.Dufendach@gmail.com>
2019-06-11 15:21:43 -0400

    initial angular fire authorization successful implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AngularFireAuth} from '@angular/fire/auth';
+import {User} from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  isLoggedIn: false;
+export class AppComponent implements OnInit {
+  isLoggedIn = false;
+  email = '';
+  pass: '';
+  user: User;
 
   constructor(public afAuth: AngularFireAuth) {}
 
-  login() {
-    console.log('login button clicked');
+  login(): void {
+    const signInPromise = this.afAuth.auth.signInWithEmailAndPassword(this.email, this.pass);
+    signInPromise.catch(e => console.log(e.message));
+  }
+
+  logout(): void {
+    this.afAuth.auth.signOut();
+  }
+
+  ngOnInit(): void {
+    this.afAuth.authState.subscribe( user => {
+      this.user = user;
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import {AngularFireAuthModule} from '@angular/fire/auth';
 import {AngularFireModule} from '@angular/fire';
 import {environment} from '../environments/environment';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import {environment} from '../environments/environment';
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireAuthModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
