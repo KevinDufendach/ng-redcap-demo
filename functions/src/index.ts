74bb2177c0ec79031ff7cae59b11b4151cd7d924
Kevin Dufendach <Kevin.Dufendach@gmail.com>
2019-06-11 14:14:14 -0400

    update index.ts to use request and config file

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,32 @@
 import * as functions from 'firebase-functions';
+import * as request from 'request-promise-native';
+import * as cfg from './config';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
+export const getTestData = functions.https.onRequest((req, resp) => {
+  const config = cfg.config;
+  const options = {
+    uri: config.uri,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Accept': 'application/json'
+    },
+    json: true, // Automatically parses the JSON string in the response
+    form: {
+      'token': config.token,
+      'content': 'project',
+      'format': 'json',
+      'returnFormat': 'json'
+    }
+  };
 
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+  const p1 = request.post(options);
+  const p2 = p1.then((getResult) => {
+    resp.send(getResult);
+  });
+  p2.catch( (error) => {
+    console.log(error);
+    resp.status(500).send(error);
+  });
 });
