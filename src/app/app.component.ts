1156a2912bf1126d356cdaeef0d52c5baf3fecdf
Kevin Dufendach <Kevin.Dufendach@gmail.com>
2019-06-11 14:57:58 -0400

    import angular fire auth module, set up environment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AngularFireAuth} from '@angular/fire/auth';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   isLoggedIn: false;
+
+  constructor(public afAuth: AngularFireAuth) {}
+
+  login() {
+    console.log('login button clicked');
+  }
 }
