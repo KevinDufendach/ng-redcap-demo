87c201da5a78399bedafbfce26b33d8d7cd44260
Kevin Dufendach <Kevin.Dufendach@gmail.com>
2019-06-20 14:03:51 -0400

    update firebase

--- functions/src/redcap-service.ts
+++ functions/src/redcap-service.ts
@@ -1,6 +1,6 @@
 import * as cfg from './config';
 import * as request from 'request-promise-native';
-import {REDCapFieldMetadata} from '../../projects/ng-redcap/src/field/field';
+import {REDCapFieldMetadata} from '../../projects/ng-redcap/src/field/redcap-field-metadata';
 
 export interface REDCapConfig {
   token: string,
@@ -37,10 +37,10 @@ export class REDCapService {
       const p1 = request.post(options);
       const p2 = p1.then((result) => {
         // ToDo: check to be sure data received are appropriately formatted
-        const resultData = <REDCapFieldMetadata[]>result;
+        const resultData = <REDCapFieldMetadata[]> result;
         resolve(resultData);
       });
-      p2.catch( (error) => {
+      p2.catch((error) => {
         console.log(error);
         reject(error);
       });
@@ -57,17 +57,17 @@ export class REDCapService {
       const p1 = request.get(options);
       const p2 = p1.then((result) => {
         // ToDo: check to be sure data received are appropriately formatted
-        const resultData = <REDCapFieldMetadata[]>result;
+        const resultData = <REDCapFieldMetadata[]> result;
         resolve(resultData);
       });
-      p2.catch( (error) => {
+      p2.catch((error) => {
         console.log(error);
         reject(error);
       });
     });
   }
 
-  getRecordExport(user: string, form?: string): Promise<object> {
+  getRecordExport(userId: string, form?: string): Promise<object> {
     const options = {
       uri: this.config.uri,
       headers: this.headers,
@@ -78,7 +78,7 @@ export class REDCapService {
         'format': 'json',
         'returnFormat': 'json',
         'type': 'flat',
-        'records[0]': user,
+        'records[0]': userId,
         'fields[0]': '',
         'forms[0]': (form || ''),
         'rawOrLabel': 'raw',
@@ -93,14 +93,50 @@ export class REDCapService {
       const p1 = request.post(options);
       const p2 = p1.then((result) => {
         // ToDo: check to be sure data received are appropriately formatted
-        const resultData = <object[]>result;
+        const resultData = <object[]> result;
         if (resultData.length < 1) {
           reject('Result not returned');
         } else {
           resolve(resultData[0]);
         }
       });
-      p2.catch( (error) => {
+      p2.catch((error) => {
+        console.log(error);
+        reject(error);
+      });
+    });
+  }
+
+  recordImport(userId: string, records: any): Promise<object> {
+    // add record_id to record
+    records['record_id'] = userId;
+
+    const options = {
+      uri: this.config.uri,
+      headers: this.headers,
+      json: true, // Automatically parses the JSON string in the response
+      form: {
+        'token': this.config.token,
+        'content': 'record',
+        'format': 'json',
+        'returnFormat': 'json',
+        'type': 'flat',
+        'data': records,
+      }
+    };
+
+    return new Promise<object>((resolve, reject) => {
+      const p1 = request.post(options);
+      const p2 = p1.then((result) => {
+        // ToDo: check to be sure data received are appropriately formatted
+        const resultData = <object[]> result;
+        if (resultData.length < 1) {
+          reject('Result not returned');
+        } else {
+          resolve(resultData[0]);
+        }
+      });
+      p2.catch((error) => {
         console.log(error);
         reject(error);
       });
